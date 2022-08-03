a15b6bbffeaabc24fe9fbcef3e3ac17092c2fa7b
AnkushinAA <r141d@mail.ru>
2022-08-03 23:18:05 +0500

    документирование, специальные комментарии

--- Logger/Console/LogToConsole.cs
+++ Logger/Console/LogToConsole.cs
@@ -6,14 +6,14 @@ public class LogToConsole : ILogger
 {
     private readonly ConsoleColorConfig _colors;
     /// <summary>
-    /// Конструктор (по умолчанию) класса LogToConsole который создает объект конфигарации
+    /// Конструктор, который создает объект конфигурации из файла по умолчанию
     /// </summary>
     public LogToConsole()
     {
         _colors = ConsoleColorConfig.Init("log_colors.json");
     }
     /// <summary>
-    /// Конструктор (с параметрами) класса LogToConsole который создает объект конфигарации, который принимает путь к файлу конфигурации
+    /// Конструктор, который принимает путь к файлу и создает объект конфигурации из него
     /// </summary>
     /// <param name="pathToColorsConfig">Путь к файлу конфигурации</param>
     public LogToConsole(string pathToColorsConfig)
@@ -21,7 +21,7 @@ public class LogToConsole : ILogger
         _colors = ConsoleColorConfig.Init(pathToColorsConfig);
     }
     /// <summary>
-    /// Статический метод вывода в консоль сообщений по принятым параметрам
+    /// Статический метод вывода в консоль сообщений по принятым параметрам с указанием даты и времени
     /// </summary>
     /// <param name="message">Текст сообщений</param>
     /// <param name="color">Цвет текста сообщений</param>
@@ -56,7 +56,7 @@ public class LogToConsole : ILogger
         WriteToConsole($"[ERROR] {message}", _colors.ColorError);
     }
     /// <summary>
-    /// Метод вывода в консоль сообщений выполнения
+    /// Метод вывода в консоль сообщений успешного выполнения
     /// </summary>
     /// <param name="message">Текст сообщения</param>
     public void Success(string message)
